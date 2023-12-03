883f23de62bfaaf0e13800f9a1df25c2b20a52b1
KenTandrian <kenricktan11@gmail.com>
2023-12-03 22:44:10 +0700

    feat(note): make title hyperlink

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -8,7 +8,9 @@ export default function NoteItem({ note, onDelete, onArchive }) {
   return (
     <div className="note-item">
       <div className="note-item__content">
-        <h3 className="note-item__title">{note.title}</h3>
+        <a href={`/notes/${note.id}`}>
+          <h3 className="note-item__title">{note.title}</h3>
+        </a>
         <p className="note-item__date">{showFormattedDate(note.createdAt)}</p>
         <p className="note-item__body">{note.body}</p>
       </div>
