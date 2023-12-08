bee5bdb381373e91e9c5d7456842f78ef52e9bb2
KenTandrian <kenricktan11@gmail.com>
2023-12-08 23:29:41 +0700

    feat(app): add 404 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,37 +2,13 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import NotFoundPage from "./pages/404";
 import AddPage from "./pages/AddPage";
 import ArchivePage from "./pages/ArchivePage";
 import DetailPage from "./pages/DetailPage";
 import HomePage from "./pages/HomePage";
-import {
-  archiveNote,
-  deleteNote,
-  getAllNotes,
-  unarchiveNote,
-} from "./utils/local-data";
 
 export default function App() {
-  function onDelete(id) {
-    const note = getAllNotes().find((note) => note.id === id);
-    if (!note) return;
-    const result = window.confirm(
-      `Are you sure you want to delete "${note.title}"?`
-    );
-    if (result) deleteNote(note.id);
-  }
-
-  function onArchive(id) {
-    const note = getAllNotes().find((note) => note.id === id);
-    if (!note) return;
-    if (note.archived) {
-      unarchiveNote(note.id);
-    } else {
-      archiveNote(note.id);
-    }
-  }
-
   return (
     <BrowserRouter>
       <div className="app-container">
@@ -42,10 +18,8 @@ export default function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/archive" element={<ArchivePage />} />
             <Route path="/new" element={<AddPage />} />
-            <Route
-              path="/notes/:id"
-              element={<DetailPage onArchive={onArchive} onDelete={onDelete} />}
-            />
+            <Route path="/notes/:id" element={<DetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
         <Footer />
--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { showFormattedDate } from "../utils";
 import { getNote } from "../utils/local-data";
+import NotFoundPage from "./404";
 
-export default function DetailPage({ onArchive, onDelete }) {
+export default function DetailPage() {
   const { id } = useParams();
   const navigate = useNavigate();
   const note = getNote(id);
+
+  function onDelete(id) {
+    const note = getAllNotes().find((note) => note.id === id);
+    if (!note) return;
+    const result = window.confirm(
+      `Are you sure you want to delete "${note.title}"?`
+    );
+    if (result) deleteNote(note.id);
+  }
+
+  function onArchive(id) {
+    const note = getAllNotes().find((note) => note.id === id);
+    if (!note) return;
+    if (note.archived) {
+      unarchiveNote(note.id);
+    } else {
+      archiveNote(note.id);
+    }
+  }
+
+  if (!note) {
+    return <NotFoundPage />;
+  }
+
   return (
     <section className="detail-page">
       <h3 className="detail-page__title">{note.title}</h3>
