8d4279f130c8926d407768482b0585a55452e0e2
KenTandrian <kenricktan11@gmail.com>
2023-12-09 20:39:54 +0700

    feat(app): get archived notes from network

--- src/pages/ArchivePage.jsx
+++ src/pages/ArchivePage.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NotesList from "../components/NotesList";
-import { getArchivedNotes } from "../utils/local-data";
+import { getArchivedNotes } from "../utils/network-data";
 
 export default function ArchivePage() {
-  const notes = getArchivedNotes();
+  const [notes, setNotes] = useState([]);
+
+  async function load() {
+    try {
+      const { data } = await getArchivedNotes();
+      if (data) setNotes(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  useEffect(() => {
+    load();
+  }, []);
+
   return <NotesList notesList={notes} title="Archived Notes" />;
 }
