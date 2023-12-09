a5b86da9d1cb6d48c64afc0e103ea5ac12241f7a
KenTandrian <kenricktan11@gmail.com>
2023-12-09 21:29:05 +0700

    fix(header): remove archive and logout button when not logged in

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,29 @@ import AppContext from "../contexts";
 
 export default function Header() {
   const navigate = useNavigate();
-  const { onLogout, theme, toggleTheme } = useContext(AppContext);
+  const { authData, onLogout, theme, toggleTheme } = useContext(AppContext);
   return (
     <div className="note-app__header">
       <h1 onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
         My Notes
       </h1>
       <div style={{ display: "flex", gap: "8px" }}>
-        <button
-          className="note-app_archive-button"
-          onClick={() => navigate("/archive")}
-        >
-          Archive
-        </button>
+        {authData.user && (
+          <button
+            className="note-app_archive-button"
+            onClick={() => navigate("/archive")}
+          >
+            Archive
+          </button>
+        )}
         <button className="note-app__icon-button" onClick={toggleTheme}>
           {theme === "dark" ? <FiSun /> : <FiMoon />}
         </button>
-        <button className="note-app__icon-button" onClick={onLogout}>
-          <FiLogOut />
-        </button>
+        {authData.user && (
+          <button className="note-app__icon-button" onClick={onLogout}>
+            <FiLogOut />
+          </button>
+        )}
       </div>
     </div>
   );
