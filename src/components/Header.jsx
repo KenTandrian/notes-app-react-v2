bd6cee4aa9f2d827a0f6d543f67787702a8ca36e
KenTandrian <kenricktan11@gmail.com>
2023-12-09 21:12:18 +0700

    feat(ui): add theme toggle

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
 import { useContext } from "react";
-import { FiLogOut } from "react-icons/fi";
+import { FiLogOut, FiMoon, FiSun } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
 import AppContext from "../contexts";
 
 export default function Header() {
   const navigate = useNavigate();
-  const { onLogout } = useContext(AppContext);
+  const { onLogout, theme, toggleTheme } = useContext(AppContext);
   return (
     <div className="note-app__header">
       <h1 onClick={() => navigate("/")} style={{ cursor: "pointer" }}>
@@ -17,6 +17,9 @@ export default function Header() {
           onClick={() => navigate("/archive")}
         >
           Archive
+        </button>
+        <button className="note-app__icon-button" onClick={toggleTheme}>
+          {theme === "dark" ? <FiSun /> : <FiMoon />}
         </button>
         <button className="note-app__icon-button" onClick={onLogout}>
           <FiLogOut />
--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -8,10 +8,13 @@ const AppContext = createContext({
   authData: DEFAULT_AUTH_DATA,
   onLogout: () => {},
   refreshAuth: () => {},
+  theme: "dark",
+  toggleTheme: () => {},
 });
 
 export function AppContextProvider({ children }) {
   const [authData, setAuthData] = useState(DEFAULT_AUTH_DATA);
+  const [theme, setTheme] = useState("dark");
 
   async function refreshAuth() {
     try {
@@ -27,12 +30,28 @@ export function AppContextProvider({ children }) {
     putAccessToken("");
   }
 
+  function toggleTheme() {
+    const currentTheme = theme;
+    setTheme((p) => (p === "light" ? "dark" : "light"));
+    localStorage.setItem("theme", currentTheme === "light" ? "dark" : "light");
+  }
+
   useEffect(() => {
     refreshAuth();
+
+    if (localStorage.getItem("theme")) {
+      setTheme(localStorage.getItem("theme"));
+    }
   }, []);
 
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
   return (
-    <AppContext.Provider value={{ authData, onLogout, refreshAuth }}>
+    <AppContext.Provider
+      value={{ authData, onLogout, refreshAuth, theme, toggleTheme }}
+    >
       {children}
     </AppContext.Provider>
   );
