e258afa50206501ab8cff3285b4d22dcc729e2a6
KenTandrian <kenricktan11@gmail.com>
2023-12-09 20:32:17 +0700

    feat(ui): add spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles/spinner.css";
+
+export default function Spinner() {
+  return (
+    <div>
+      <div className="loading-spinner" />
+    </div>
+  );
+}
--- src/pages/LoadingPage.jsx
+++ src/pages/LoadingPage.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import Spinner from "../components/Spinner";
 
 export default function LoadingPage() {
   return (
     <section className="loading-page">
+      <Spinner />
       <h2>Loading...</h2>
     </section>
   );
